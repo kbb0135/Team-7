5be060d0bd2d4b95fcd6ca5a95c8abada2c396a3
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-04-04 00:54:54 -0500

    Sign Up function started

---
+++ unt-canteen/src/SignUp.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './style.css';
+
+function SignUp() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmEmail, setConfirmEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle sign up logic
+  }
+
+  return (
+    <div className="signup">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" id="firstName" name="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+        <label htmlFor="lastName">Last Name:</label>
+        <input type="text" id="lastName" name="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <label htmlFor="confirmEmail">Confirm Email:</label>
+        <input type="email" id="confirmEmail" name="confirmEmail" value={confirmEmail} onChange={(e) => setConfirmEmail(e.target.value)} required />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input type="password" id="confirmPassword" name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required />
+        <input type="submit" value="Sign Up" />
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
