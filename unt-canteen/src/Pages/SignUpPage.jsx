45f88803d8237c149459179cf07f028b568289c3
Julian Mendoza <julianmendoza728@yahoo.com>
2023-04-20 14:12:52 -0500

    Created the Model and Component folder. Created the home Model and view

--- unt-canteen/src/Pages/SignUpPage.jsx
+++ unt-canteen/src/Pages/SignUpPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../Header.jsx";
 import Footer from "../Footer.jsx";
-import SignUp from "../SignUp.jsx";
+import SignUp from "../Models/SignUp.jsx";
 
 const SignUpPage = () => {
     return(
