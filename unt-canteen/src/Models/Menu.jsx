fb56ba622c2eab4403f10ee3d4acc10d6eac34e5
Julian Mendoza <julianmendoza728@yahoo.com>
2023-05-02 00:42:44 -0500

    Added more cards to menu.jsx for testing Added :hover to Header_btn for visual effects Addded flex-wrap to .MenuBody and margins to the .card. As well as cursor effects during over methods

--- unt-canteen/src/Models/Menu.jsx
+++ unt-canteen/src/Models/Menu.jsx
@@ -23,6 +23,60 @@ function Menu() {
             rating="1.5"
             location="Discovery Parks Market"
             imageSrc={Coke}/>
+            <Card 
+            title="Coke"
+            price="$1.50"
+            rating="5.0"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Sushi"
+            price="$3.50"
+            rating="4.0"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Rice"
+            price="$6.50"
+            rating="1.5"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Coke"
+            price="$1.50"
+            rating="5.0"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Sushi"
+            price="$3.50"
+            rating="4.0"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Rice"
+            price="$6.50"
+            rating="1.5"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Coke"
+            price="$1.50"
+            rating="5.0"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Sushi"
+            price="$3.50"
+            rating="4.0"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
+            <Card 
+            title="Rice"
+            price="$6.50"
+            rating="1.5"
+            location="Discovery Parks Market"
+            imageSrc={Coke}/>
         </div>
     )
  
