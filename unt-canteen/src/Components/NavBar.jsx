cb62a9ea2cd2f922ba617cb6ff0d30672dbfd0a5
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-05-05 18:55:17 -0500

    updation in login and signup

--- unt-canteen/src/Components/NavBar.jsx
+++ unt-canteen/src/Components/NavBar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <Link to ="/home"><li className="nav-item">Home</li></Link>
-        <Link to = "/menu"><li className="nav-item">Menu</li></Link>
-        <Link to = "/maps"><li classsName="nav-item">Maps</li></Link>
-        <Link to = "/hoursandlocation"><li className="nav-item">Hours And Location</li></Link>
-        <Link to = "/recreationactivities"><li className="nav-item">Recreation Activities</li></Link>
+        <Link to ="/home" className= "nav-head"><li className="nav-item">Home</li></Link>
+        <Link to = "/menu"className= "nav-head"><li className="nav-item">Menu</li></Link>
+        {/* <Link to = "/maps"className= "nav-head"><li classsName="nav-item">Maps</li></Link> */}
+        <Link to = "/hoursandlocation" className= "nav-head"><li className="nav-item">Hours And Location</li></Link>
+        <Link to = "/recreationactivities" className= "nav-head"><li className="nav-item">Recreation Activities</li></Link>
       </ul>
       
     </nav>
