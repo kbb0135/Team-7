d9017472e98e8ba4eb23fa7d3c774146d3178d58
Budhathoki, Karan <budhathokikaran135@gmail.com>
2023-05-05 18:32:00 -0500

    merging into local master

--- unt-canteen/src/Components/NavBar.jsx
+++ unt-canteen/src/Components/NavBar.jsx
@@ -1,16 +1,16 @@
 
 import React from 'react';
 import '../style.css'; // Import CSS file for Navbar component
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <li className="nav-item"><a href="#home">Home</a></li>
-        <li className="nav-item"><a href="#about">Menu</a></li>
-        <li classsName="nav-item"><a href="#contact">Maps</a></li>
-        <li className="nav-item"><a href="#contact">Hours And Location</a></li>
-        <li className="nav-item"><a href="#contact">Recreation Activities</a></li>
+        <Link to ="/home"><li className="nav-item">Home</li></Link>
+        <Link to = "/menu"><li className="nav-item">Menu</li></Link>
+        <Link to = "/maps"><li classsName="nav-item">Maps</li></Link>
+        <Link to = "/hoursandlocation"><li className="nav-item">Hours And Location</li></Link>
+        <Link to = "/recreationactivities"><li className="nav-item">Recreation Activities</li></Link>
       </ul>
     </nav>
   );
--- unt-canteen/src/Models/Login.jsx
+++ unt-canteen/src/Models/Login.jsx
@@ -1,13 +1,38 @@
 import React, {useState} from 'react';
 import '../style.css';
-import { Link } from 'react-router-dom';
+import { auth, db } from "../firebase";
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+;
+
+
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleSubmit = (event) => {
-        event.preventDefault();
+      event.preventDefault();
         // handle sign up logic
+      const email = event.target[0].value;
+      const password = event.target[1].value; 
+
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log("user")
+        navigate("/home")        
+        // ...
+      })
+    .catch((error) => {
+    const errorMessage = error.message;
+    alert(errorMessage)
+  });
+
+
+
       }
 
   return (
@@ -19,7 +44,7 @@ function Login() {
         <label htmlFor="password">Password:</label>
         <input type="password" id="password" className="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         <input type="submit" value="LogIn" /> 
-        <Link to="/signup" className="direct"><h4 className="direct">Back to SignUp</h4></Link>
+        <Link to="/home" className="direct"><h4 className="direct">Back to SignUp</h4></Link>
       </form>
     </div>
   );
